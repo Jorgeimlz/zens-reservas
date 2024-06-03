022bba3777262ad311a7c3eef8987991f4634347
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-06-03 16:02:55 -0500

    instalacion de xlsx para exportar excel

--- src/components/verReservas.js
+++ src/components/verReservas.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './styles/verReservas.css';
+import * as XLSX from 'sheetjs-style';
+import { saveAs } from 'file-saver';
 
 const VerReservas = () => {
   const [reservas, setReservas] = useState([]);
@@ -49,6 +51,25 @@ const VerReservas = () => {
     return reservas.filter(reserva => reserva.idFiesta === idFiesta);
   };
 
+  const exportToExcel = (reservas, nombreFiesta) => {
+    const wb = XLSX.utils.book_new();
+    const wsData = reservas.map(reserva => ({
+      Hora: reserva.hora,
+      Nombre: `${reserva.nombreReserva} ${reserva.apellidoReserva}`,
+      'Número de Personas': reserva.numeroPersonas,
+      Teléfono: reserva.telefono,
+    }));
+    const ws = XLSX.utils.json_to_sheet(wsData);
+    XLSX.utils.book_append_sheet(wb, ws, nombreFiesta);
+    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+    const buf = new ArrayBuffer(wbout.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < wbout.length; i++) {
+      view[i] = wbout.charCodeAt(i) & 0xFF;
+    }
+    saveAs(new Blob([buf], { type: "application/octet-stream" }), `Reservas_${nombreFiesta}.xlsx`);
+  };
+
   return (
     <div className="background">
       <div className="mainPage">
@@ -56,18 +77,22 @@ const VerReservas = () => {
         <button className="reservar-button" onClick={handleButtonClick}>Ver Reservas</button>
         {mostrarReservas && fiestas.length > 0 && (
           <div className="columnas">
-            {fiestas.map((fiesta, index) => (
-              <div className={`columna columna${index + 1}`} key={fiesta.idFiesta}>
-                <h3>{fiesta.nombreFiesta}</h3>
-                {clasificarReservas(reservas, fiesta.idFiesta).map((reserva) => (
-                  <ul key={reserva.idReserva}>
-                    <h4>{reserva.hora} - {reserva.nombreReserva} {reserva.apellidoReserva}</h4>
-                    <li>Número de Personas: {reserva.numeroPersonas}</li>
-                    <li>Teléfono: {reserva.telefono}</li>
-                  </ul>
-                ))}
-              </div>
-            ))}
+            {fiestas.map((fiesta, index) => {
+              const reservasFiesta = clasificarReservas(reservas, fiesta.idFiesta);
+              return (
+                <div className={`columna columna${index + 1}`} key={fiesta.idFiesta}>
+                  <h3>{fiesta.nombreFiesta}</h3>
+                  {reservasFiesta.map((reserva) => (
+                    <ul key={reserva.idReserva}>
+                      <h4>{reserva.hora} - {reserva.nombreReserva} {reserva.apellidoReserva}</h4>
+                      <li>Número de Personas: {reserva.numeroPersonas}</li>
+                      <li>Teléfono: {reserva.telefono}</li>
+                    </ul>
+                  ))}
+                  <button className="reservar-button" onClick={() => exportToExcel(reservasFiesta, fiesta.nombreFiesta)}>Exportar</button>
+                </div>
+              );
+            })}
           </div>
         )}
       </div>
